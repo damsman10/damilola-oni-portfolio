be7d02bc1ddf52e7d2902c1e1912387bf3c2dea1
Damilola <damilolaoni07@gmail.com>
2024-09-10 13:57:11 +0100

    updated sidebar

--- src/sticky/Fixedside.jsx
+++ src/sticky/Fixedside.jsx
@@ -4,7 +4,7 @@ import NavMenu from "./stickycomps/NavMenu"
 
 const Fixedside = () => {
   return (
-    <div className="h-[100vh] w-[20%]" style={{backgroundColor: "#040B14"}}>
+    <div className="h-[100vh] w-[17rem] border-r border-white" style={{backgroundColor: "#040B14"}}>
         <ImageSocials />
         <NavMenu />
     </div>
